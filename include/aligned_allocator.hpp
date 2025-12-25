d51f3417f02f2080c255e6b5b17620293084c6ff
James <jameslimbj@gmail.com>
2025-12-25 18:18:04 +0800

    refactor: rewrite allocator for familiarity

--- include/aligned_allocator.hpp
+++ include/aligned_allocator.hpp
@@ -2,31 +2,32 @@
 
 #include <cstddef>
 #include <new>
+#include <type_traits>
 
-template <class T, std::size_t Alignment>
+template<typename T, std::size_t Alignment>
 struct aligned_allocator {
-  using value_type = T;
+    static_assert(Alignment >= sizeof(T));
+    static_assert((Alignment & (Alignment - 1)) == 0);
 
-  aligned_allocator() noexcept = default;
-  template <class U>
-  aligned_allocator(const aligned_allocator<U, Alignment>&) noexcept {}
+    using value_type = T;
 
-  [[nodiscard]] T* allocate(std::size_t n) {
-    std::size_t bytes = n * sizeof(T);
-    void* p = ::operator new(bytes, std::align_val_t(Alignment));
-    return static_cast<T*>(p);
-  }
+    aligned_allocator() noexcept = default;
+    
+    template<typename U>
+    aligned_allocator(const aligned_allocator<U, Alignment>&) {}
 
-  void deallocate(T* p, std::size_t) noexcept {
-    ::operator delete(p, std::align_val_t(Alignment));
-  }
+    [[nodiscard]] T* allocate(std::size_t n) {
+        std::size_t bytes = n * sizeof(T);
+        void* p = ::operator new(bytes, std::align_val_t(Alignment));
+        return static_cast<T*>(p);
+    }
+    
+    void deallocate(T* p, std::size_t) noexcept {
+        ::operator delete(p, std::align_val_t(Alignment));
+    }
 
-  template <class U>
-  struct rebind { using other = aligned_allocator<U, Alignment>; };
+    template<typename U>
+    struct rebind { using other = aligned_allocator<U, Alignment>; };
+
+    using is_always_equal = std::true_type;
 };
-
-template <class T1, std::size_t A1, class T2, std::size_t A2>
-bool operator==(const aligned_allocator<T1, A1>&, const aligned_allocator<T2, A2>&) { return A1 == A2; }
-
-template <class T1, std::size_t A1, class T2, std::size_t A2>
-bool operator!=(const aligned_allocator<T1, A1>& a, const aligned_allocator<T2, A2>& b) { return !(a == b); }
